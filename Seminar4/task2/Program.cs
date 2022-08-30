33250a8fcea29f502a70be4357325d3ae8de7fde
Liliia-M <smak0991@mail.ru>
2022-08-30 12:07:55 +0300

    task2

---
+++ Seminar4/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNum(int num)
+{
+   int ost= 0;
+   int result = 0;
+   while(num>0)
+   {
+    ost = num%10;
+    
+    if(num>10) 
+    {
+        result = result + ost;
+    }     
+    else
+    {
+        result = result + num;
+    }
+    
+    num = num/10;
+   }
+   return result;    
+}
+
+Console.WriteLine("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{GetNum(number)}");
