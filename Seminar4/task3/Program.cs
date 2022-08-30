4b57a8a291be2cdaf0066b5092207c3db8e1e9e9
Liliia-M <smak0991@mail.ru>
2022-08-30 13:34:33 +0300

    task 3_29

---
+++ Seminar4/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайнми числами из [a, b) и выводит их на экран.
+// 5, 0, 20 -> [1, 2, 5, 7, 19]
+// 3, 1, 35 -> [6, 1, 33]
+
+Console.WriteLine("Введите число N, соответствующее числу элементов массива: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число a, соответствующее возможному наименьшему из массива: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b, соответствующее возможному наибольшему из массива: ");
+int b = int.Parse(Console.ReadLine());
+
+int [] InitArray()
+{
+    int[] arr = new int[N];
+    for(int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(a, b+1);
+    }
+    return arr;
+}
+
+Console.WriteLine(String.Join(", ", InitArray()));
+
+
