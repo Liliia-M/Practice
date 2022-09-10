58208ae3491d777b1a8f28f364a41dc6e2f0f401
Liliia-M <smak0991@mail.ru>
2022-09-10 20:26:00 +0300

    task 41

---
+++ Seminar6/task1_41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int PositiveNum(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите M чисел: ");
+int M = int.Parse(Console.ReadLine());
+int[] myArray = GetArray(M,-100, 100);
+Console.WriteLine(String.Join(" ", myArray));
+Console.WriteLine(PositiveNum(myArray));
