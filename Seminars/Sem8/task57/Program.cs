6332ae0aace7455a7920554b0c5b440286efe4c5
Liliia-M <smak0991@mail.ru>
2022-09-14 16:34:56 +0300

    task 57

---
+++ Seminars/Sem8/task57/Program.cs
@@ -0,0 +1,106 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+
+// 0 встречается 2 раза 
+// 1 встречается 1 раз 
+// 2 встречается 1 раз 
+// 8 встречается 1 раз 
+// 9 встречается 3 раза
+
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] ConvertOneDArray(int[,] array)
+{
+    int[] newArray = new int [array.GetLength(0)*array.GetLength(1)];
+    int k = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray[k] = array[i, j];
+            k++;
+        }
+    }
+    return newArray;
+    
+}
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array [minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void PrintNumbers(int[] array)
+{
+    int count = 1;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (array[i] == array[i+1])
+        {
+            count++;
+        }
+        else
+        {
+            Console.WriteLine($"{array[i]} встречается {count} раз(а)");
+            count = 1;
+        }
+    }
+    Console.WriteLine($"{array[array.Length - 1]} встречается {count} раз(а)");
+}
+
+int[,] myArray = GetArray(2, 5, 0, 10);
+PrintArray(myArray);
+Console.WriteLine();
+
+int[] newArray = ConvertOneDArray(myArray);
+SelectionSort(newArray);
+Console.WriteLine(String.Join(" ", newArray));
+
+PrintNumbers(newArray);
